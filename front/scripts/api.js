a1a907867a5079dfa468de3f3b2436fcbba17229
ArayaCG <arayag64@gmail.com>
2024-03-11 15:54:15 -0300

    Uso de async y await

--- front/scripts/api.js
+++ front/scripts/api.js
@@ -1,16 +1,19 @@
 //se llama al objeto Repository
 const { Repository } = require("./objetos.js");
+const axios = require("axios");
 
-function llenarRepositorioDesdeAPI() {
+async function llenarRepositorioDesdeAPI() {
     const repository = new Repository();
-
-    $.get("https://students-api.2.us-1.fl0.io/movies", (data) => {
+    try {
+        data = await axios.get("https://students-api.2.us-1.fl0.io/movies").data;
         data.forEach(({ title, year, director, duration, genre, rate, poster }) => {
             repository.createFilm(title, year, director, duration, genre, rate, poster);
         });
         // Una vez que se llenan los datos desde la API, llamamos a la función para actualizar las tarjetas
         require("./tarjetas.js").instanciarContendor(repository.getAllFilms());
-    });
+    } catch (error) {
+        console.error(error);
+    }
 }
 //se exporta la función
 module.exports = { llenarRepositorioDesdeAPI };
