6062ec433d9fe97f4fe9cb42ceeb7e1662028a7d
ArayaCG <arayag64@gmail.com>
2024-03-14 12:25:59 -0300

    conectar back con api

--- back/src/controllers/moviesController.js
+++ back/src/controllers/moviesController.js
@@ -2,10 +2,10 @@ const movieService = require("../services/getMoviesService");
 
 const movieController = async (req, res) => {
     try {
-        const movies = await movieService.getMoviesService(); 
+        const movies = await movieService();
         res.status(200).json(movies);
     } catch (error) {
-        res.status(500).json({ error: error.message }); 
+        res.status(500).json({ error: error.mesagge });
     }
 };
 
--- back/src/services/getMoviesService.js
+++ back/src/services/getMoviesService.js
@@ -1,35 +1,12 @@
-module.exports = {
-    getMoviesService: async () => {
-        const moviesData = [
-            {
-                title: "Guardians of the Galaxy Vol. 2",
-                year: 2017,
-                director: "James Gunn",
-                duration: "2h 16min",
-                genre: ["Action", "Adventure", "Comedy"],
-                rate: 7.7,
-                poster: "https://m.media-amazon.com/images/M/MV5BNjM0NTc0NzItM2FlYS00YzEwLWE0YmUtNTA2ZWIzODc2OTgxXkEyXkFqcGdeQXVyNTgwNzIyNzg@._V1_SX300.jpg",
-            },
-            {
-                title: "Star Wars: Episode IV - A New Hope",
-                year: 1977,
-                director: "George Lucas",
-                duration: "2h 1min",
-                genre: ["Action", "Adventure", "Fantasy", "Sci-Fi"],
-                rate: 8.7,
-                poster: "https://m.media-amazon.com/images/M/MV5BOTA5NjhiOTAtZWM0ZC00MWNhLThiMzEtZDFkOTk2OTU1ZDJkXkEyXkFqcGdeQXVyMTA4NDI1NTQx._V1_SX300.jpg",
-            },
-            {
-                title: "The Lord of the Rings: The Fellowship of the Ring",
-                year: 2001,
-                director: "Peter Jackson",
-                duration: "2h 58min",
-                genre: ["Action", "Adventure", "Drama", "Fantasy"],
-                rate: 8.8,
-                poster: "https://m.media-amazon.com/images/M/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_SX300.jpg",
-            },
-        ];
+const axios = require("axios");
 
-        return moviesData;
-    },
+const moviesService = async () => {
+    try {
+        const respuesta = await axios("https://students-api.up.railway.app/movies");
+        return respuesta.data;
+    } catch (error) {
+        console.log(error.message);
+    }
 };
+
+module.exports = moviesService;
